82a372ee8cf3cbb65d0c1a7296249ac192027688
abhir20 <37292638+abhir20@users.noreply.github.com>
2024-04-12 22:19:05 -0400

    Added two new programs

---
+++ redundantBraces.js
@@ -0,0 +1,8 @@
+//return 1 if A has redundant braces, else 0
+//I/P : ((a+b)) , returns 1
+//keep pushing until you find an operator
+
+const redundantBraces = () =>{
+
+};
+
--- validParantheses.js
+++ validParantheses.js
@@ -14,4 +14,50 @@
 //Input: {()
 //O/p : false
 
+const isValid = (str) => {
 
+    let stack = [];
+
+    const mapping = {
+        "}" : "{",
+        "]" : "[",
+        ")" : "("
+    };
+
+    const openBracketCheck = "({[";
+
+    for(let bracket of str){
+        if(openBracketCheck.includes(bracket)){
+            stack.push(bracket);
+        }
+        if(stack.length > 0){
+           
+            if(mapping[bracket] === stack[stack.length - 1]){ // if open and close brackets match
+                
+                stack.pop();
+                return true;
+            }
+        }
+        else
+        {
+            return false
+        }
+       
+    }
+
+    return stack.length === 0;
+}
+
+//To check if corresponding brackets are properly closed
+var isValidCorresponding = function(s) {
+    let obj={'(':')','{':'}','[':']'}
+    let stack=[]
+    for(let char of s){
+        if(obj[char]) stack.push(char)
+        else if(obj[stack.pop()]!==char) return false
+    }
+    return stack.length==0
+};
+
+const str = "([)]";
+console.log(isValid(str));
